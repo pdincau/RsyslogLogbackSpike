671d6ffc308d4ec30a2131a6df2cdd1e22e0ea14
Paolo D'Incau <paolo.dincau@xpeppers.com>
2016-09-10 11:40:13 +0200

    Initial commit

---
+++ src/main/java/LogbackSpike.java
@@ -0,0 +1,12 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogbackSpike {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LogbackSpike.class);
+
+    public static void main(String[] args) {
+        LOG.info("this is just a spike");
+    }
+
+}
